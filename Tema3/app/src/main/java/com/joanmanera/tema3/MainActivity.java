2683ffa0307414ffa25a9973f97b22fe35936328
Acer <joanet31@hotmail.com>
2019-09-30 17:52:31 +0200

    comit3

--- Tema3/app/src/main/java/com/joanmanera/tema3/MainActivity.java
+++ Tema3/app/src/main/java/com/joanmanera/tema3/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button boton;
-    private EditText tvNombre;
-    private EditText tvApellidos;
+    private EditText etNombre;
+    private EditText etApellidos;
 
 
     @Override
@@ -20,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvNombre = findViewById(R.id.etNombre);
-        tvApellidos = findViewById(R.id.etApellidos);
+        etNombre = findViewById(R.id.etNombre);
+        etApellidos = findViewById(R.id.etApellidos);
         boton = findViewById(R.id.bSaluda);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                String mensajeSaludo = "Hola "+tvNombre.getText().toString() + " " + tvApellidos.getText().toString();
+                String mensajeSaludo = "Hola "+etNombre.getText().toString() + " " + etApellidos.getText().toString();
                 Toast.makeText(MainActivity.this, mensajeSaludo, Toast.LENGTH_LONG).show();
             }
         });
